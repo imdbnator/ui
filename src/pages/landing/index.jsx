e907dbed04d5e5a55ad121a6589d922bd3b3b5fa
Sai Krishna Deep <saikrishna2deep@gmail.com>
2017-10-04 16:45:53 +0530

    Fiing mongodb error

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -353,20 +353,20 @@ export default class Landing extends React.Component {
       typeSpeed: 100
     })
 
-    fetch(`//${process.env.API_HOST}/collection?date=1`, {
-      method: 'get',
-    })
-    .then(function (response) {
-      if (response.status !== 200) throw new Error(`API server ${response.status} status error.`)
-      return response.json()
-    })
-    .then(data => {
-      if (!data.success) throw new Error(data.message)
-      debug && console.log('Stats (INFO):', data)
-      this.setState({stats: data.stats})
-    })
-    .catch((err) => {
-      debug && console.log(err.message)
-    })
+    // fetch(`//${process.env.API_HOST}/collection?date=1`, {
+    //   method: 'get',
+    // })
+    // .then(function (response) {
+    //   if (response.status !== 200) throw new Error(`API server ${response.status} status error.`)
+    //   return response.json()
+    // })
+    // .then(data => {
+    //   if (!data.success) throw new Error(data.message)
+    //   debug && console.log('Stats (INFO):', data)
+    //   this.setState({stats: data.stats})
+    // })
+    // .catch((err) => {
+    //   debug && console.log(err.message)
+    // })
   }
 }
